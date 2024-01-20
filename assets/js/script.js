74e8416ff932268e2a8b5e1dd55b541a760e7b41
12kharris Harris <kurtharris1541@gmail.com>
2024-01-20 14:12:20 +0000

    Add more questions and score holders

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // create an object to hold all possible questions
     let questions = [];
 
+    // CREATE A QUESTIONS CLASS THEN INITIALISE NEW QUESTIONS?
     let questionOne = {
         questionText: "QUESTION 1 TEXT",
         options: [
@@ -23,20 +24,109 @@ document.addEventListener("DOMContentLoaded", function () {
         ]
     };
 
+    let questionThree = {
+        questionText: "QUESTION 3 TEXT",
+        options: [
+            { option: "3OPTION1", correct: 1 },
+            { option: "3OPTION2", correct: 0 },
+            { option: "3OPTION3", correct: 0 },
+            { option: "3OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionFour = {
+        questionText: "QUESTION 4 TEXT",
+        options: [
+            { option: "4OPTION1", correct: 1 },
+            { option: "4OPTION2", correct: 0 },
+            { option: "4OPTION3", correct: 1 },
+            { option: "4OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionFive = {
+        questionText: "QUESTION 5 TEXT",
+        options: [
+            { option: "5OPTION1", correct: 1 },
+            { option: "5OPTION2", correct: 0 },
+            { option: "5OPTION3", correct: 0 },
+            { option: "5OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionSix = {
+        questionText: "QUESTION 6 TEXT",
+        options: [
+            { option: "6OPTION1", correct: 0 },
+            { option: "6OPTION2", correct: 0 },
+            { option: "6OPTION3", correct: 1 },
+            { option: "6OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionSeven = {
+        questionText: "QUESTION 7 TEXT",
+        options: [
+            { option: "7OPTION1", correct: 0 },
+            { option: "7OPTION2", correct: 0 },
+            { option: "7OPTION3", correct: 1 },
+            { option: "7OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionEight = {
+        questionText: "QUESTION 8 TEXT",
+        options: [
+            { option: "8OPTION1", correct: 0 },
+            { option: "8OPTION2", correct: 0 },
+            { option: "8OPTION3", correct: 1 },
+            { option: "8OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionNine = {
+        questionText: "QUESTION 9 TEXT",
+        options: [
+            { option: "9OPTION1", correct: 0 },
+            { option: "9OPTION2", correct: 0 },
+            { option: "9OPTION3", correct: 1 },
+            { option: "9OPTION4", correct: 0 }
+        ]
+    };
+
+    let questionTen = {
+        questionText: "QUESTION 10 TEXT",
+        options: [
+            { option: "10OPTION1", correct: 0 },
+            { option: "10OPTION2", correct: 0 },
+            { option: "10OPTION3", correct: 1 },
+            { option: "10OPTION4", correct: 0 }
+        ]
+    };
+
     // Array to hold order of questions
     let questionPositions = [];
     // Generate random positions for questions which must be unique
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 10; i++) {
         let rand;
         do {
-            rand = Math.floor(Math.random() * 2);
+            rand = Math.floor(Math.random() * 10);
         } while (questionPositions.includes(rand));
         questionPositions[i] = rand;
     }
 
     // questionPositions[i] is a random int so question order now random
+    // There must be abetter way of doing this. MAYBE ADD THE QUESTIONS TO AN ENUMERABLE AND ITERATE?
     questions[questionPositions[0] + 1] = questionOne;
     questions[questionPositions[1] + 1] = questionTwo;
+    questions[questionPositions[2] + 1] = questionThree;
+    questions[questionPositions[3] + 1] = questionFour;
+    questions[questionPositions[4] + 1] = questionFive;
+    questions[questionPositions[5] + 1] = questionSix;
+    questions[questionPositions[6] + 1] = questionSeven;
+    questions[questionPositions[7] + 1] = questionEight;
+    questions[questionPositions[8] + 1] = questionNine;
+    questions[questionPositions[9] + 1] = questionTen;
 
     let questionNumber = 1;
 
@@ -91,10 +181,14 @@ function checkAnswer(questionNumber, questions) {
     }
 
     if (selectedOption.getAttribute("data-type") === "1") {
-        console.log("CORRECT!");
+        let scoreHolder = document.getElementById("score");
+        let score = scoreHolder.innerText;
+        scoreHolder.innerText = ++score;
     }
     else {
-        console.log("WRONG!");
+        let incorrectHolder = document.getElementById("incorrect-count");
+        let count = incorrectHolder.innerText;
+        incorrectHolder.innerText = ++count;
     }
 
     //console.log(`Correct? ${}`);
